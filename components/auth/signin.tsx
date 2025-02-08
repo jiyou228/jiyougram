bc82dc3a163bc00fb0fb2f98556f1e7a70300712
jiyou228 <jhjy504@naver.com>
2025-02-08 14:11:19 +0900

    로그인 formik, yup을 이용하여 검사

--- components/auth/signin.tsx
+++ components/auth/signin.tsx
@@ -1,13 +1,37 @@
 "use client";
 import { useMutation } from "@tanstack/react-query";
 import { Button, Input } from "@material-tailwind/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { createBrowserSupabaseClient } from "utils/supabase/client";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 export default function SignIn({ setView }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
   const supabase = createBrowserSupabaseClient();
+
+  const validationSchema = useMemo(() => {
+    return Yup.object().shape({
+      email: Yup.string()
+        .email("이메일 형식이 올바르지 않습니다.")
+        .required("이메일을 입력해주세요."),
+      password: Yup.string()
+        .min(8, "비밀번호는 최소 8자리 입니다.")
+        .required("비밀번호를 입력해주세요."),
+    });
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      signInMutation.mutate();
+    },
+  });
 
   const signInWithKaKao = async () => {
     const {} = await supabase.auth.signInWithOAuth({
@@ -23,8 +47,8 @@ export default function SignIn({ setView }) {
   const signInMutation = useMutation({
     mutationFn: async () => {
       const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
+        email: formik.values.email,
+        password: formik.values.password,
       });
       if (data) {
         console.log(data);
@@ -36,28 +60,36 @@ export default function SignIn({ setView }) {
   });
 
   return (
-    <div className="flex flex-col gap-4">
+    <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
       <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-lg border border-gray-400 bg-white gap-2">
         <img src={"/images/inflearngram.png"} className="w-60 mb-6" />
+
         <Input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={formik.values.email}
+          onChange={formik.handleChange}
           label="email"
+          name="email"
           type="email"
           className="w-full rounded-sm"
         />
+        {formik.errors.email && (
+          <span className="text-red-700">{formik.errors.email}</span>
+        )}
 
         <Input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formik.values.password}
+          onChange={formik.handleChange}
           label="password"
           type="password"
+          name="password"
           className="w-full rounded-sm"
         />
+        {formik.errors.password && (
+          <span className="text-red-700">{formik.errors.password}</span>
+        )}
+
         <Button
-          onClick={() => {
-            signInMutation.mutate();
-          }}
+          type="submit"
           loading={signInMutation.isPending}
           disabled={signInMutation.isPending}
           color="light-blue"
@@ -82,6 +114,6 @@ export default function SignIn({ setView }) {
           가입하기
         </button>
       </div>
-    </div>
+    </form>
   );
 }
